f66fa002641856bdeb35a6c5ecbf68bfa7e8e9f8
JenChieh <jcs090218@gmail.com>
2025-01-01 02:02:39 -0800

    Update copyright year

--- ansi-colorful.el
+++ ansi-colorful.el
@@ -1,6 +1,6 @@
 ;;; ansi-colorful.el --- Toggle render ansi-color  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Shen, Jen-Chieh
+;; Copyright (C) 2024-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
